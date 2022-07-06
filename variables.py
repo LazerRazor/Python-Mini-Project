52124f9b5eed3f8efa59e751bdda8c72cb5322ad
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2022-07-06 20:36:19 +0530

    Created variables file for importing

---
+++ variables.py
@@ -0,0 +1,32 @@
+X = 1280
+Y = 720
+Menu = True
+Moment = False
+Kinematics = False
+
+MomDefault = True
+MomElement = False
+MomMoment = False
+MomForce = False
+
+KinDefault = True
+KinProjectile = False
+KinVariable = False
+KinProjectileSim = False
+
+KinVelocity = False
+KinRadMass = False
+Solve = False
+StructureExists = False
+Count = 0
+user_text = ''
+ind  = 0
+count = 0
+x1,x2,y1,y2 = False, False, False, False
+NodeCheck = False
+ShowProjectile = False
+ShowMotion = False
+RR = 0
+Solution = False
+
+a, b, c = False, False, False
