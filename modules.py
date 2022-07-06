ca9d50a0d5929ce80244f6e12f19a441d101a4f7
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2022-07-06 18:36:10 +0530

    Added all module imports to modules.py

---
+++ modules.py
@@ -0,0 +1,12 @@
+import pygame, assets.colors as col, assets.quit as Q , matplotlib
+from assets.button import Button 
+from assets.rectangle import Rectangle
+import matplotlib.backends.backend_agg as agg
+from anastruct import SystemElements
+import matplotlib.pyplot as plt
+import pymunk
+import pymunk.pygame_util
+import math
+from backend.projectileMotion import Projectile
+from backend.variableMotion import Equation
+import pylab
